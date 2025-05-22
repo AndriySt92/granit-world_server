20912a2af61ed736b46c8f653f55a5afc2b13435
AndriySt92 <andriyua-1992@mail.ru>
2025-05-22 21:03:37 +0300

    feat(middleware): add errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,33 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
 import connectDB from "./config/connectDb";
+import { errorHandler } from "./middlewares";
+import { CustomError } from "./utils";
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.all("*", (req, _res, next): void => {
+  const error = new CustomError(`Route ${req.originalUrl} not found`, 404);
+  next(error);
+});
+
+app.use(errorHandler);
 
 // Database connection
 connectDB();
